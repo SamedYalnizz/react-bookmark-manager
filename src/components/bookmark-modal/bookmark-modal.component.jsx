7284cec4db2fd89581f79828ed279b51837a8083
Samed Yalniz <yalniz.samed@gmail.com>
2020-09-07 20:32:33 +0300

    adding fontawesome

--- src/components/bookmark-modal/bookmark-modal.component.jsx
+++ src/components/bookmark-modal/bookmark-modal.component.jsx
@@ -1,15 +1,23 @@
 import React, { Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faWindowClose} from '@fortawesome/free-solid-svg-icons';
 import './bookmark-modal.style.css';
 
 export const BookmarkModal = () => (
     <div>
         <div>
             <h2>Add New Bookmark</h2>
-            <i className="fas fa-window-close"></i>
+            <FontAwesomeIcon icon={faWindowClose}/>
+        </div>
+        <div>
+            
         </div>
         Bookmark Modal
     </div>
 )
+
+// Create Modal Header (Title & Close Icon)
+// Need BookmarkForm
 
 
 // header title, close button
